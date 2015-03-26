498f4d1914c66330af91755d5335e7e73841a8a0
Jacob Clark <jacob.clark@bbc.co.uk>
2015-03-26 11:02:57 +0000

    Refactoring

--- test/TestSpec.js
+++ test/TestSpec.js
@@ -4,52 +4,32 @@ var assert 	= 	require("assert"),
 describe('Stack should', function(){
     before(function() {
         this.stack = new Stack();
+
+        this.firstObject    = "first object";
+        this.secondObject   = "second object";
     });
 
     it('pop the most recently pushed object', function(){
-        // Given we have an object to push to the stack
-        var objectToPush = new Object;
+        this.stack.push(this.firstObject);
 
-        // When we push the object to the stack
-        this.stack.push(objectToPush);
+        assert.equal(this.stack.pop(), this.firstObject);	
 
-        // Then pop should return the object pushed onto the stack
-        var pop = this.stack.pop();
-        assert.equal(typeof pop, 'object');	
-
-        // And once the stack is empty, it should throw an exception
-        assert.throws(pop, Error, "Array is empty");	
+        assert.throws(this.stack.pop, Error, "Array is empty");	
     });
 
     it('pop objects in the reverse order to which they were pushed', function(){
-        // Given we have several objects to push to the stack
-        var firstObject     = 	new Object,
-            secondObject    =	new Object,
-            thirdObject     =	new Object;
+        this.stack.push(this.firstObject);
+        this.stack.push(this.secondObject);
 
-        // When we push the objects to the stack
-        this.stack.push(firstObject);
-        this.stack.push(secondObject);
-        this.stack.push(thirdObject);
-
-        // Then pop should return an object type as many times as an object has been pushed
-        var pop = this.stack.pop();
-
-        assert.equal(typeof pop, 'object');
-        assert.equal(typeof pop, 'object');
-        assert.equal(typeof pop, 'object');
-
-        // And once the stack is empty, it should throw an exception
-        assert.throws(pop, Error, "Array is empty");					
+        assert.equal(this.stack.pop(), this.secondObject);
+        assert.equal(this.stack.pop(), this.firstObject);
+        
+        assert.throws(this.stack.pop, Error, "Array is empty");					
     });
 
     it('throw an exception if popped when empty', function(){
-        // Given we have a new, empty stack
         this.stack = new Stack();
-
-        // When we do nothing to the stack
-
-        // Then pop should thrown an 'Array is empty' exception, because the Stack is empty
+        
         assert.throws(this.stack.pop, Error, "Array is empty");			
     })
 })
