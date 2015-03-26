7cedc9979548a269a7a6506820df05cc38703d9b
Jacob Clark <jacob.clark@bbc.co.uk>
2015-03-26 10:32:33 +0000

    Refactoring the tests

--- test/TestSpec.js
+++ test/TestSpec.js
@@ -2,18 +2,28 @@ var assert = require("assert")
 var Stack  =require('../src/Stack');
 
 describe('Stack should', function(){
+	before(function() {
+		this.stack = new Stack();
+	});
+
 	it('pop most recently pushed object', function(){
-		var stack = new Stack();
+		// Given we have an object to push to the stack
 		var stringToPush = "Hello World";
 
-		stack.push(stringToPush);
+		// When we push the object to the stack
+		this.stack.push(stringToPush);
 
-		assert.equal(stack.pop(), stringToPush);			
+		// Pop should return the object pushed onto the stack
+		assert.equal(this.stack.pop(), stringToPush);			
 	});
 
 	it('throw an exception if popped when empty', function(){
-		var stack = new Stack();
-		
-		assert.throws(stack.pop, Error, "Array is empty");			
+		// Given we have a new, empty stack
+		this.stack = new Stack();
+
+		// When we do nothing to the stack
+
+		// Pop should thrown an 'Array is empty' exception, because the Stack is empty
+		assert.throws(this.stack.pop, Error, "Array is empty");			
 	})
 })
