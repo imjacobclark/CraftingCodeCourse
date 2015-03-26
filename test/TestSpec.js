fabcfdcb4f13724b7957437e8daca03e792a7f7f
Jacob Clark <jacob.clark@bbc.co.uk>
2015-03-26 10:45:07 +0000

    Test Refactoring

--- test/TestSpec.js
+++ test/TestSpec.js
@@ -1,20 +1,46 @@
-var assert = require("assert")
-var Stack  =require('../src/Stack');
+var assert 	= 	require("assert"),
+	Stack  	=	require('../src/Stack');
 
 describe('Stack should', function(){
 	before(function() {
 		this.stack = new Stack();
 	});
 
-	it('pop most recently pushed object', function(){
+	it('pop the most recently pushed object', function(){
 		// Given we have an object to push to the stack
-		var stringToPush = "Hello World";
+		var objectToPush = new Object;
 
 		// When we push the object to the stack
-		this.stack.push(stringToPush);
+		this.stack.push(objectToPush);
 
-		// Pop should return the object pushed onto the stack
-		assert.equal(this.stack.pop(), stringToPush);			
+		// Then pop should return the object pushed onto the stack
+		var pop = this.stack.pop();
+		assert.equal(typeof pop, 'object');	
+
+		// And once the stack is empty, it should throw an exception
+		assert.throws(pop, Error, "Array is empty");	
+	});
+
+	it('pop objects in the reverse order to which they were pushed', function(){
+		// Given we have several objects to push to the stack
+		var firstObject 	= 	new Object,
+			secondObject	=	new Object,
+			thirdObject		=	new Object;
+
+		// When we push the objects to the stack
+		this.stack.push(firstObject);
+		this.stack.push(secondObject);
+		this.stack.push(thirdObject);
+
+		// Then pop should return an object type as many times as an object has been pushed
+		var pop = this.stack.pop();
+
+		assert.equal(typeof pop, 'object');
+		assert.equal(typeof pop, 'object');
+		assert.equal(typeof pop, 'object');
+
+		// And once the stack is empty, it should throw an exception
+		assert.throws(pop, Error, "Array is empty");					
 	});
 
 	it('throw an exception if popped when empty', function(){
@@ -23,7 +49,7 @@ describe('Stack should', function(){
 
 		// When we do nothing to the stack
 
-		// Pop should thrown an 'Array is empty' exception, because the Stack is empty
+		// Then pop should thrown an 'Array is empty' exception, because the Stack is empty
 		assert.throws(this.stack.pop, Error, "Array is empty");			
 	})
 })
